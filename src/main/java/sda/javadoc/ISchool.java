e8eb5735073a4b2a03cf0a838af94859583fa020
T-Viking <olafw96@gmail.com>
2019-03-03 11:56:35 +0100

    mod 11

--- src/main/java/sda/javadoc/ISchool.java
+++ src/main/java/sda/javadoc/ISchool.java
@@ -4,8 +4,34 @@ import java.util.List;
 import java.util.Set;
 
 public interface ISchool {
+    /**
+     * Adds student given in a parameter to the school.
+     * @param student - student added to school.
+     */
     void addStudent(Student student);
+
+    /**
+     * Creates and adds student to school. Method requires all parameters to create single student
+     * than adds newly created student to school.
+     * @param firstName - first name of the student that is going to be created.
+     * @param lastName - last name of the student that is going to be created.
+     * @param age - age of the student that is going to be created.
+     * @param courses - courses attended by the student that is going to be created.
+     */
     void addStudent(String firstName, String lastName, int age, Set<Course> courses);
+
+    /**
+     * Uses student by first name and last name parameters.
+     * @param firstName - first name of the student being used.
+     * @param lastName - last name of the student being used
+     * @return
+     */
     Student getStudentByFirstNameAndLastName(String firstName, String lastName);
+
+    /**
+     * Creates a list based on courses attended by students.
+     * @param course - courses attended by students.
+     * @return
+     */
     List<Student> getStudentsByCourse(Course course);
 }
--- src/main/java/sda/javadoc/SDAcademySchool.java
+++ src/main/java/sda/javadoc/SDAcademySchool.java
@@ -3,15 +3,43 @@ package sda.javadoc;
 import java.util.List;
 import java.util.Set;
 
+/**
+ * Adds students to the school using ISchool index.
+ */
 public class SDAcademySchool implements ISchool {
+    /**
+     * Adds student to the school.
+     * @param student - student added to school.
+     */
     @Override
     public void addStudent(Student student) {}
+
+    /**
+     *Adds student's firstName, lastName,age,courses to the school listing.
+     * @param firstName - first name of the student that is going to be created.
+     * @param lastName - last name of the student that is going to be created.
+     * @param age - age of the student that is going to be created.
+     * @param courses - courses attended by the student that is going to be created.
+     */
     @Override
     public void addStudent(String firstName, String lastName, int age, Set<Course> courses) {}
+
+    /**
+     * Adds student to the school by first and last name.
+     * @param firstName - first name of the student being used.
+     * @param lastName - last name of the student being used
+     * @return - returns imaginary value.
+     */
     @Override
     public Student getStudentByFirstNameAndLastName(String firstName, String lastName) {
         return null;
     }
+
+    /**
+     *
+     * @param course - courses attended by students.
+     * @return - returns imaginary value.
+     */
     @Override
     public List<Student> getStudentsByCourse(Course course) {
         return null;
